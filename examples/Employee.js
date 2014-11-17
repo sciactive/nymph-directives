c33aeae424ae23a7154addd223d6f2420d5d7229
Hunter Perrin <hperrin@gmail.com>
2014-11-17 11:13:14 -0800

    Added an example of date input support.

--- examples/Employee.js
+++ examples/Employee.js
@@ -13,7 +13,10 @@
 		this.constructor.call(this, id);
 		this.addTag('employee');
 		this.data.current = true;
+		this.data.start_date = Math.floor(new Date().getTime() / 1000);
 		this.data.subordinates = [];
+		this.start_date = this.start_date.bind(this);
+		this.end_date = this.end_date.bind(this);
 	};
 	Employee.prototype = new Entity();
 
@@ -22,7 +25,29 @@
 		class: 'Employee',
 
 		// === Class Variables ===
-		etype: "employee"
+		etype: "employee",
+		storeStartDate: null,
+		storeEndDate: null,
+
+		// === Class Methods ===
+		start_date: function(date){
+			if (angular.isDefined(date)) {
+				this.storeStartDate = date;
+				this.data.start_date = Math.floor(date.getTime() / 1000);
+			}
+			if (this.storeStartDate === null && this.data.start_date)
+				this.storeStartDate = new Date(this.data.start_date * 1000);
+			return this.storeStartDate;
+		},
+		end_date: function(date){
+			if (angular.isDefined(date)) {
+				this.storeEndDate = date;
+				this.data.end_date = Math.floor(date.getTime() / 1000);
+			}
+			if (this.storeEndDate === null && this.data.end_date)
+				this.storeEndDate = new Date(this.data.end_date * 1000);
+			return this.storeEndDate;
+		}
 	};
 	for (var p in thisClass) {
 		Employee.prototype[p] = thisClass[p];
